be4c173463e1fb631592975301dfe6e3951cf31f
Daco2020 <dacokim32@gmail.com>
2023-12-27 15:15:37 +0900

    chore: 데이터 정렬 추가

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/supabaseClient.js";
 
 export async function load() {
-    const { data } = await supabase.from("projects").select().eq('is_active', true);;
+    const { data } = await supabase.from("projects").select().eq('is_active', true).order('created_at', {ascending: false});;
     return {
         projects: data ?? [],
     };

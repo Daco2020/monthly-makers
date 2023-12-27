75f48e715a0368504ce5a7387e526fd2a112881a
Daco2020 <dacokim32@gmail.com>
2023-12-27 10:57:37 +0900

    feat: 조회 데이터로 상세페이지 표시

--- src/routes/projects/[projectId]/+page.server.js
+++ src/routes/projects/[projectId]/+page.server.js
@@ -3,7 +3,5 @@ import { supabase } from "$lib/supabaseClient.js";
 export async function load( {params} ) {
     const id = params.projectId;     
     const { data } = await supabase.from("projects").select().eq("id", id).single();
-    return {
-        project: data,
-    };
+    return data;
 }
--- src/routes/projects/[projectId]/+page.svelte
+++ src/routes/projects/[projectId]/+page.svelte
@@ -1,38 +1,39 @@
 <script>
 	export let data;
-	console.log(data);
+
+	import { userStore } from '../../../stores/userStore';
+
+	$: user = $userStore;
 </script>
 
-<h1 class="text-4xl text-center my-8 font-bold">프로젝트 이름</h1>
-
-<div class="min-h-screen flex flex-col items-center justify-center">
+<div classsingle="min-h-screen flex flex-col items-center justify-center">
 	<!-- Detail Page Container -->
 	<div class="bg-white rounded-lg shadow-lg overflow-hidden w-full max-w-2xl mx-auto">
 		<!-- Image -->
-		<img
-			src="https://source.unsplash.com/random/400x300"
-			alt="Detail"
-			class="w-full h-64 object-cover"
-		/>
+		<img src={data.thumbnail} alt="Detail" class="w-full h-96 object-contain" />
 		<!-- Content -->
 		<div class="p-4">
-			<h1 class="text-2xl font-semibold mb-2">타이틀</h1>
-			<h2 class="text-lg text-gray-700 mb-4">한줄 소개</h2>
-			<p class="text-gray-700 mb-4">상세 설명</p>
-			<a href="#" class="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out"
-				>링크</a
+			<h1 class="text-2xl font-semibold mb-2">{data.title}</h1>
+			<h2 class="text-lg text-gray-700 mb-4">{data.description}</h2>
+			<p class="text-gray-700 mb-4">{data.detail}</p>
+			<a
+				href={data.link}
+				class="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out"
+				>프로젝트 보러가기</a
 			>
 			<!-- Buttons -->
-			<div class="flex mt-4">
-				<button
-					class="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-red-600 transition duration-300 ease-in-out"
-					>삭제버튼</button
-				>
-				<button
-					class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease-in-out"
-					>수정버튼</button
-				>
-			</div>
+			{#if user && user.id === data.user_id}
+				<div class="flex mt-4">
+					<button
+						class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease-in-out"
+						>수정</button
+					>
+					<button
+						class="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-red-600 transition duration-300 ease-in-out"
+						>삭제</button
+					>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>

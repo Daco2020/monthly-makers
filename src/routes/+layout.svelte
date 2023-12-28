f23b8d91ba1942ea8f95723cec26377660ee5be2
Daco2020 <dacokim32@gmail.com>
2023-12-28 18:52:25 +0900

    chore: 파일 경로 변경

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.css';
-	import Nav from '../components/Nav.svelte';
+	import Nav from '../components/nav.svelte';
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
 	import { userStore } from '../stores/userStore';
